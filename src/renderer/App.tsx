ac139f3c8631ed46c8a4b0cb5a1de148b7a4660c
Adam Zvěřina <adam.zverina@gmail.com>
2023-04-27 09:28:18 +0200

    Added default home page, ignore for newline characters

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,19 +1,12 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import './css/style.css';
-
-function Hello() {
-  return (
-    <div>
-
-    </div>
-  );
-}
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
